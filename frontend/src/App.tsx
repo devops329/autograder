c0f02d263ed62192af163849fc7ad83ed7dfb25d
Stephen Amos <amos.stephen.m@gmail.com>
2024-06-18 15:56:32 -0600

    fix login issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,19 +2,30 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Grader } from './components/grader/Grader';
 import { NavBar } from './components/navbar/NavBar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { UserInfo } from './components/userInfo/UserInfo';
 import { Submissions } from './components/submissions/Submissions';
 import { Admin } from './components/admin/Admin';
 import { User } from './model/domain/User';
 import { Submission } from './model/domain/Submission';
 import { Login } from './components/login/Login';
+import Cookies from 'js-cookie';
 
 function App() {
   const [loggedInUser, setLoggedInUser] = useState<User | null>(localStorage.getItem('user') ? User.fromJson(JSON.parse(localStorage.getItem('user')!)) : null);
   const [submissions, setSubmissions] = useState<Submission[]>(
     localStorage.getItem('submissions') ? JSON.parse(localStorage.getItem('submissions')!).map((item: JSON) => Submission.fromJson(item)) : []
   );
+
+  useEffect(() => {
+    if (!Cookies.get('token') && loggedInUser) {
+      localStorage.removeItem('user');
+      localStorage.removeItem('submissions');
+      setLoggedInUser(null);
+      setSubmissions([]);
+      window.location.href = '/';
+    }
+  }, []);
 
   return (
     <>
