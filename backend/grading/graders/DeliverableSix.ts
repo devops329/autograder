a7a4d5bd2335131d24dcfa82d5b15a56b963016c
Stephen Amos <amos.stephen.m@gmail.com>
2024-07-19 15:44:07 -0600

    update d6 grading

--- backend/grading/graders/DeliverableSix.ts
+++ backend/grading/graders/DeliverableSix.ts
@@ -3,9 +3,33 @@ import { Github } from '../tools/Github';
 import { GradingTools } from '../tools/GradingTools';
 import { Grader } from './Grader';
 
+/*
+| Percent | Item                                                    |
+| ------- | ------------------------------------------------------- |
+| 20%     | Secure Fargate deployment based on ECR and ECS          |
+| 20%     | AWS Load balancer used to access Fargate                |
+| 20%     | MySQL database deployed for backend data persistence    |
+| 30%     | Updated GitHub Action workflow deploying to ECR and ECS |
+| 10%     | Your backend called for all frontend requests           |
+*/
+interface Rubric {
+  ecrEcsFargateDeployment: number;
+  awsLoadBalancer: number;
+  mySQLDatabase: number;
+  githubActionWorkflow: number;
+  ownBackendCalledForFrontendRequests: number;
+}
+
 export class DeliverableSix implements Grader {
-  async grade(user: User): Promise<[number]> {
+  async grade(user: User): Promise<[number, object]> {
     let score = 0;
+    const rubric: Rubric = {
+      ecrEcsFargateDeployment: 0,
+      awsLoadBalancer: 0,
+      mySQLDatabase: 0,
+      githubActionWorkflow: 0,
+      ownBackendCalledForFrontendRequests: 0,
+    };
     const github = new Github(user, 'jwt-pizza');
     const tools = new GradingTools();
 
@@ -19,11 +43,18 @@ export class DeliverableSix implements Grader {
 
     // Check for successful run
     const runSuccess = await github.checkRecentRunSuccess('ci.yml');
-    if (pushesToECS && buildsAndPushesToECR && runSuccess) score += 40;
+    if (pushesToECS && buildsAndPushesToECR && runSuccess) {
+      score += 20;
+      rubric.ecrEcsFargateDeployment += 20;
+      rubric.githubActionWorkflow += 20;
+    }
 
     // Check DNS
     const deployedWithELB = await tools.checkDNS(user.website, /elb\.amazonaws\.com/);
-    if (deployedWithELB) score += 20;
+    if (deployedWithELB) {
+      score += 20;
+      rubric.awsLoadBalancer += 20;
+    }
 
     // Get service url
     const envFile = await github.readGithubFile('.env.production');
@@ -33,9 +64,13 @@ export class DeliverableSix implements Grader {
     // Use curl to create a user and then login in as the user
     if (usingOwnService) {
       const serviceWorks = await tools.createUserAndLogin(serviceUrl);
-      if (serviceWorks) score += 40;
+      if (serviceWorks) {
+        score += 30;
+        rubric.ownBackendCalledForFrontendRequests += 10;
+        rubric.mySQLDatabase += 20;
+      }
     }
 
-    return [score];
+    return [score, rubric];
   }
 }
