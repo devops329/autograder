ff5963038e4b42b5385010b97207f4b9a6495bd3
Stephen Amos <amos.stephen.m@gmail.com>
2024-07-22 13:45:20 -0600

    feat: add grade comments to d6

--- backend/grading/graders/DeliverableSix.ts
+++ backend/grading/graders/DeliverableSix.ts
@@ -4,15 +4,6 @@ import { Github } from '../tools/Github';
 import { GradingTools } from '../tools/GradingTools';
 import { Grader } from './Grader';
 
-/*
-| Percent | Item                                                    |
-| ------- | ------------------------------------------------------- |
-| 20%     | Secure Fargate deployment based on ECR and ECS          |
-| 20%     | AWS Load balancer used to access Fargate                |
-| 20%     | MySQL database deployed for backend data persistence    |
-| 30%     | Updated GitHub Action workflow deploying to ECR and ECS |
-| 10%     | Your backend called for all frontend requests           |
-*/
 interface Rubric {
   ecrEcsFargateDeployment: number;
   awsLoadBalancer: number;
@@ -41,46 +32,54 @@ export class DeliverableSix implements Grader {
     const pushesToECS = workflowFile.includes('aws-actions/amazon-ecs-deploy-task-definition');
     const buildsAndPushesToECR = workflowFile.includes('docker build') && workflowFile.includes('$ECR_REGISTRY/$ECR_REPOSITORY --push');
 
-    // Run the workflow
-    // await github.triggerWorkflowAndWaitForCompletion('ci.yml');
+    if (pushesToECS && buildsAndPushesToECR) {
+      // Run the workflow
+      await github.triggerWorkflowAndWaitForCompletion('ci.yml');
 
-    // Check for successful run
-    const runSuccess = await github.checkRecentRunSuccess('ci.yml');
-    if (pushesToECS && buildsAndPushesToECR && runSuccess) {
-      score += 20;
-      rubric.ecrEcsFargateDeployment += 20;
-      rubric.githubActionWorkflow += 20;
-    } else {
-      rubric.comments += 'Your GitHub Action workflow did not deploy to ECR and ECS successfully.\n';
-    }
+      // Check for successful run
+      const runSuccess = await github.checkRecentRunSuccess('ci.yml');
+      if (runSuccess) {
+        score += 50;
+        rubric.ecrEcsFargateDeployment += 20;
+        rubric.githubActionWorkflow += 30;
+      } else {
+        rubric.comments += 'Your GitHub Action workflow did not complete successfully.\n';
+      }
 
-    // Check DNS
-    const deployedWithELB = await tools.checkDNS(user.website, /elb\.amazonaws\.com/);
-    if (deployedWithELB) {
-      score += 20;
-      rubric.awsLoadBalancer += 20;
-    }
+      // Check DNS
+      const deployedWithELB = await tools.checkDNS(user.website, /elb\.amazonaws\.com/);
+      if (deployedWithELB) {
+        score += 20;
+        rubric.awsLoadBalancer += 20;
+      } else {
+        rubric.comments += 'Your website is not deployed with an AWS Load Balancer.\n';
+      }
 
-    // Get service url from frontend
-    github.setRepo('jwt-pizza');
-    const envFile = await github.readGithubFile('.env.production');
-    const serviceUrl = await tools.getEnvVariable(envFile, 'VITE_PIZZA_SERVICE_URL');
-    if (!serviceUrl) {
-      rubric.comments += 'Could not find VITE_PIZZA_SERVICE_URL in .env.production.\n';
-      return [score, rubric];
-    }
-    const usingOwnService = !serviceUrl.includes('cs329.click');
+      // Get service url from frontend
+      github.setRepo('jwt-pizza');
+      const envFile = await github.readGithubFile('.env.production');
+      const serviceUrl = await tools.getEnvVariable(envFile, 'VITE_PIZZA_SERVICE_URL');
+      if (!serviceUrl) {
+        rubric.comments += 'Could not find VITE_PIZZA_SERVICE_URL in .env.production.\n';
+        return [score, rubric];
+      }
+      const usingOwnService = !serviceUrl.includes('cs329.click');
 
-    // Use curl to create a user and then login in as the user
-    if (usingOwnService) {
-      const serviceWorks = await tools.createUserAndLogin(serviceUrl);
-      if (serviceWorks) {
-        score += 30;
-        rubric.ownBackendCalledForFrontendRequests += 10;
-        rubric.mySQLDatabase += 20;
+      // Use curl to create a user and then login in as the user
+      if (usingOwnService) {
+        const serviceWorks = await tools.createUserAndLogin(serviceUrl);
+        if (serviceWorks) {
+          score += 30;
+          rubric.ownBackendCalledForFrontendRequests += 10;
+          rubric.mySQLDatabase += 20;
+        } else {
+          rubric.comments += 'Your service does not work as expected.\n';
+        }
+      } else {
+        rubric.comments += 'You are using the provided service URL.';
       }
     } else {
-      rubric.comments += 'You are using the provided service URL.';
+      rubric.comments += 'Your GitHub Action workflow does not build and push to ECR and deploy to ECS.\n';
     }
 
     return [score, rubric];
