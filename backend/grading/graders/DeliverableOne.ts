8a38c67756be9c801b9de27b13b245f91c06b934
Stephen Amos <amos.stephen.m@gmail.com>
2024-12-26 13:05:56 -0700

    Ref: rewrite d1 to basic checks for repo and notes file

--- backend/grading/graders/DeliverableOne.ts
+++ backend/grading/graders/DeliverableOne.ts
@@ -1,57 +1,40 @@
 import { User } from '../../model/domain/User';
+import { Github } from '../tools/Github';
 import { GradingTools } from '../tools/GradingTools';
 import { Grader } from './Grader';
 
 interface DeliverableOneRubric {
-  customDomainName: number;
-  githubPages: number;
+  repoExists: number;
+  taAddedAsCollaborator: number;
   comments: string;
 }
 
 export class DeliverableOne implements Grader {
   private tools: GradingTools;
+  private github: Github = new Github();
 
   constructor(tools: GradingTools) {
     this.tools = tools;
   }
   async grade(user: User, gradeAttemptId: string): Promise<[number, DeliverableOneRubric]> {
+    let score = 0;
     const rubric: DeliverableOneRubric = {
-      customDomainName: 0,
-      githubPages: 0,
+      repoExists: 0,
+      taAddedAsCollaborator: 0,
       comments: '',
     };
-    const hostname = user.website;
 
-    let score = 0;
-
-    if (!hostname) {
-      rubric.comments += 'No website provided.\n';
+    // Read notes.md file from student's fork of jwt-pizza repo
+    const notesFile = await this.github.readGithubFile(user, 'jwt-pizza', 'notes.md', gradeAttemptId);
+    if (!notesFile) {
+      rubric.comments = 'Could not read notes.md file';
       return [0, rubric];
     }
+    rubric.repoExists += 40;
+    score += 40;
 
-    let customDomainNameSuccess = false;
-    let githubPagesSuccess = false;
+    // Check that the TA has been added as a collaborator
 
-    customDomainNameSuccess = await this.tools.checkPageBodyForText(hostname, /JWT Pizza/g);
-    githubPagesSuccess = await this.tools.checkDNS(hostname, /github\.io/, gradeAttemptId);
-
-    if (customDomainNameSuccess) {
-      score += 30;
-      rubric.customDomainName += 30;
-    } else {
-      rubric.comments += 'JWT Pizza is not functional at the provided website.\n';
-    }
-
-    if (githubPagesSuccess) {
-      if (customDomainNameSuccess) {
-        score += 40;
-        rubric.githubPages += 40;
-      }
-      score += 30;
-      rubric.githubPages += 30;
-    } else {
-      rubric.comments += 'Your website is not hosted by GitHub Pages.\n';
-    }
     return [score, rubric];
   }
 }
